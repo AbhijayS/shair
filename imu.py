78803ebbfce5952add10031cfbd5730bb4837d59
Abhijay Saini <abhijay.saini@gmail.com>
2021-01-12 22:58:48 -0600

    integrated 2d with 3d tracking (untested)

---
+++ imu.py
@@ -0,0 +1,136 @@
+import serial
+import time
+from ahrs.filters import Madgwick
+from ahrs import Quaternion
+import numpy as np
+import math
+import operator
+from config import ARDUINO_COM_PORT, ARDUINO_BAUD_RATE
+
+class IMU:
+    # constants
+    FREQUENCY = 100
+    GAIN = 0.2
+    GRAVITY = 9.80665
+    SENSOR_SIGN = [1,1,1,-1,-1,-1,1,1,1] # order: gyrxyz, acclxyz, magxyz
+    # order: x y z
+    MAG_HARD_IRON = [-341.618045, -3822.569674, 4116.527667]
+    # matrix copied from magneto
+    MAG_SOFT_IRON = [1.030425, 0.007653, -0.004353,
+                    0.007653, 1.046725, -0.006299,
+                    -0.004353, -0.006299, 1.091559]
+
+    def __init__(self):
+        ser = serial.Serial(ARDUINO_COM_PORT, ARDUINO_BAUD_RATE)
+        self.ser_buff = ''
+
+        # clean up input buffer
+        timeout = time.time()+1
+        while time.time() < timeout:
+            ser.read_all()
+
+        self.Q = [1.0, 0.0, 0.0, 0.0]
+        self.madwick = Madgwick()
+        # self.madwick.frequency = self.FREQUENCY
+        self.madwick.beta = self.GAIN
+        self.timestamp = 0
+
+        # calculate offsets
+        ACC_OFFSETS = [0]*3 #xyz
+        GYR_OFFSETS = [0]*3 #xyz
+        for i in range(32):
+            data = ser.readline().decode('utf-8').split(',')
+            ax,ay,az = list(map(lambda x: self.accel_meter_per_sec(int(x)), data[1:4]))
+            gx,gy,gz = list(map(lambda x: self.gyro_rad_per_sec(int(x)), data[4:7]))
+            GYR_OFFSETS[0] = GYR_OFFSETS[0] + gx
+            GYR_OFFSETS[1] = GYR_OFFSETS[1] + gy
+            GYR_OFFSETS[2] = GYR_OFFSETS[2] + gz
+            ACC_OFFSETS[0] = ACC_OFFSETS[0] + ax
+            ACC_OFFSETS[1] = ACC_OFFSETS[1] + ay
+            ACC_OFFSETS[2] = ACC_OFFSETS[2] + az
+        GYR_OFFSETS = list(map(lambda x: x/32, GYR_OFFSETS))
+        ACC_OFFSETS = list(map(lambda x: x/32, ACC_OFFSETS))
+
+        self.ACC_OFFSETS = ACC_OFFSETS
+        self.GYR_OFFSETS = GYR_OFFSETS
+        self.ser = ser
+
+        # initalize imu
+        timeout = time.time()+10
+        while time.time() < timeout:
+            self.update()
+        
+        print("IMU ready to go!")
+
+    # https://www.pololu.com/file/0J1087/LSM6DS33.pdf
+    def accel_meter_per_sec(self, a_raw):
+        return a_raw * 0.061 * self.GRAVITY / 1000
+
+    # https://www.pololu.com/file/0J1087/LSM6DS33.pdf
+    def gyro_rad_per_sec(self, g_raw):
+        return math.radians(g_raw * 8.75 / 1000)
+
+    # https://www.pololu.com/file/0J1089/LIS3MDL.pdf
+    def mag_milli_tesla(self, m_raw):
+        return m_raw / 68420.0
+    
+    # call this update loop at least self.FREQUENCY or better
+    def update(self):
+        if self.ser.in_waiting:
+            raw = self.ser.read_all().decode('utf-8')
+
+            if '\n' in raw:
+                raw = raw.split('\n')
+                if len(raw) > 2:
+                    data = (raw[-2]).split(',')
+                else:
+                    data = (self.ser_buff + raw[-2]).split(',')
+                self.ser_buff = raw[-1]
+
+                if len(data) != 10:
+                    print(data)
+                    raise Exception("Serial data ill-formatted")
+                    
+                t = int(data[0])
+
+                self.madwick.Dt = (t-self.timestamp)/1000
+
+                # TODO: use numpy here; make it faster
+                # a = accel_meter_per_sec(sign*(a_raw-offset))
+                ax,ay,az = list(map(lambda x : self.accel_meter_per_sec(x), map(operator.mul, self.SENSOR_SIGN[3:6], map(operator.sub, map(lambda x: int(x), data[1:4]), self.ACC_OFFSETS))))
+                # g = gyro_rad_per_sec(sign*(a_raw-offset))
+                gx,gy,gz = list(map(lambda x : self.gyro_rad_per_sec(x), map(operator.mul, self.SENSOR_SIGN[0:3], map(operator.sub, map(lambda x: int(x), data[4:7]), self.GYR_OFFSETS))))
+                # ax,ay,az = list(map(lambda x: accel_meter_per_sec(int(x)), data[1:4]))
+                # gx,gy,gz = list(map(lambda x: gyro_rad_per_sec(int(x)), data[4:7]))
+
+                # m = matrix*(m-bias)
+                mx,my,mz = list(map(operator.sub, map(lambda x: int(x), data[7:10]), self.MAG_HARD_IRON))
+                mx = mx*self.MAG_SOFT_IRON[0] + my*self.MAG_SOFT_IRON[1] + mz*self.MAG_SOFT_IRON[2]
+                my = mx*self.MAG_SOFT_IRON[3] + my*self.MAG_SOFT_IRON[4] + mz*self.MAG_SOFT_IRON[5]
+                mz = mx*self.MAG_SOFT_IRON[6] + my*self.MAG_SOFT_IRON[7] + mz*self.MAG_SOFT_IRON[8]
+                mx,my,mz = list(map(lambda x: self.mag_milli_tesla(int(x)), (mx,my,mz)))
+
+                self.timestamp = t
+                self.Q = self.madwick.updateMARG(self.Q, gyr=np.array([gx,gy,gz]), acc=np.array([ax,ay,az]), mag=np.array([mx,my,mz]))
+            else:
+                self.ser_buff = self.ser_buff + raw
+
+    def get_euler_angles(self):
+        return list(map(lambda x : math.degrees(x), Quaternion(self.Q).to_angles()))
+
+def main():
+    myIMU = IMU()
+
+    # clear log file
+    with open('imu.data', 'w') as f:
+        pass
+
+    while True:
+        myIMU.update()
+        with open('imu.data', 'a') as f:
+            euler = myIMU.get_euler_angles()
+            f.write(str(euler[0]) + "," + str(euler[1]) + "," + str(euler[2]) + "\n")
+            # print(euler)
+        
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -8,13 +8,15 @@ from multiprocessing import Process
 from config import OPTICAL_DPI, ARDUINO_COM_PORT
 import csv
 from imu import IMU
+import struct
+import numpy as np
 
 # init IMU connection
 imu = IMU()
 
-# reset main.csv
-with open('main.csv', 'w') as f:
-    fieldnames = ['timestamp', 'pose_x', 'pose_y', 'pose_yaw', 'dx', 'dy', 'loop']
+# reset clipper.csv
+with open('clipper.csv', 'w') as f:
+    fieldnames = ['timestamp', 'pose2d_x', 'pose2d_y', 'pose2d_yaw', 'pose3d_x', 'pose3d_y', 'pose3d_z', 'bearing', 'dx', 'dy', 'loop']
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -23,7 +25,7 @@ file_position = 0
 def get_displacement_vector():
     global file_position
     try:
-        with open("mouse.data", "r") as f:
+        with open("mouse.csv", "r") as f:
             lines = f.readlines()[file_position:]
             file_position = file_position+len(lines)
             x = 0
@@ -32,29 +34,28 @@ def get_displacement_vector():
                 dx, dy = line.split(',')
                 x = x + int(dx)
                 y = y - int(dy)
-            return (x/OPTICAL_DPI, y/OPTICAL_DPI)
+            return [x/OPTICAL_DPI, y/OPTICAL_DPI]
     except IOError:
         # this should never happen
         # fix if it does
-        print("mouse.data unavailable")
+        print("mouse.csv unavailable")
         return (0, 0)
 
 # returns two angles whose difference is minimized: (a,b)
-# assumes difference is +-180
+# assumes max difference is +-180
 def shortest_angle(angle_1, angle_2):
     diff = angle_2-angle_1
     return (angle_1, angle_2-360) if diff > 180 else (angle_1-360, angle_2) if diff < -180 else (angle_1, angle_2)
 
 
 # pose exponential using numerical integration
-# assuming constant curvature, angular rate, and velocity
+# assuming constant curvature, angular rate, and speed
 # TODO: upload derivation
-def global_translation(x, y, theta_1_deg, theta_2_deg):
+def translation_2d(x, y, theta_1_deg, theta_2_deg):
     a,b = shortest_angle(theta_1_deg, theta_2_deg)
     a = radians(a)
     b = radians(b)
     delta_theta = b-a
-
     if delta_theta == 0:
         return (
             y*cos(a) + x*cos(a-pi/2),
@@ -65,41 +66,284 @@ def global_translation(x, y, theta_1_deg, theta_2_deg):
         -((cos(b)-cos(a))*y+(sin(b)-sin(a))*x)/delta_theta
     )
 
-# constants
-# ALPHA = 0.15
-RADIUS = 4.5/2.54 # 4.5 cm to in
+# TODO: speed up reading of large files
+#       checking for redundant points is too inefficient
+#           maybe try constructing triangles using redundant points?
+def read_stl(filename):
+    '''
+    returns:
+        - tuple of points, normals, and triangles
+        - points, normals, and triangles are numpy arrays
+        - points.shape => (num_points, 3)
+        - normals.shape => (num_normals, 3)
+        - triangles.shape => (num_triangles, 3)
+            - each triangle contains 3 point indices (referencing the points array)
+    '''
+
+    with open(filename, 'rb') as f:
+        f.seek(80)
+        [facets,] = struct.unpack('I', f.read(4))
+        points = []
+        triangles = []
+        normals = []
+
+        for i in range(facets):
+            normal = list(struct.unpack('fff', f.read(12)))
+            normals.append(normal)
+
+            v1 = list(struct.unpack('fff', f.read(12)))
+            v2 = list(struct.unpack('fff', f.read(12)))
+            v3 = list(struct.unpack('fff', f.read(12)))
+            triangle = []
+
+            if v1 in points:
+                triangle.append(points.index(v1))
+            else:
+                triangle.append(len(points))
+                points.append(v1)
+
+            if v2 in points:
+                triangle.append(points.index(v2))
+            else:
+                triangle.append(len(points))
+                points.append(v2)
+
+            if v3 in points:
+                triangle.append(points.index(v3))
+            else:
+                triangle.append(len(points))
+                points.append(v3)
+
+            triangles.append(triangle)
+            f.seek(2, 1)
+    return (np.array(points), np.array(normals), np.array(triangles))
+
+
+
+def transform_coordinates(local_units, global_vector):
+  # params:
+    # local_units := local unit vectors expressed using the global system
+    # global_vector := vector to be converted into local system
+  # returns: local vector coordinates in local units
+  global_units = np.array([
+    [1,0,0], # i-hat
+    [0,1,0], # j-hat
+    [0,0,1]  # k-hat
+  ])
+  return np.matmul(np.dot(global_units, local_units), global_vector)
+
+def point_local_to_global(local_units, origin_point, local_point):
+  return origin_point + local_units[0]*local_point[0] + local_units[1]*local_point[1] + local_units[2]*local_point[2]
+
+def vector_local_to_global(local_units, local_vector):
+  return np.sum(local_units*local_vector, axis=0)
+
+def intersection_point(p1,p2,p3,p4):
+  # params:
+    # line 1: p1(x1,y1), p2(x2,y2)
+    # line 2: p3(x3,y3), p4(x4,y4)
+  # returns
+    # (int, (x,y))
+    # int:
+      # 0 if no intersection
+      # 1 if 1 intersection
+      # 2 if coincident
+    # x,y: x-y coordinates of intersection point
+  denom = (p4[1]-p3[1])*(p2[0]-p1[0])-(p4[0]-p3[0])*(p2[1]-p1[1])
+  numa = (p4[0]-p3[0])*(p1[1]-p3[1])-(p4[1]-p3[1])*(p1[0]-p3[0])
+  numb = (p2[0]-p1[0])*(p1[1]-p3[1])-(p2[1]-p1[1])*(p1[0]-p3[0])
+  if denom == 0:
+    # parallel
+    if numa == 0 and numb == 0:
+      # coincident
+      return (2,)
+    return (0,)
+  else:
+    ua = numa/denom
+    ub = numb/denom
+    if ua>0 and ua<1 and ub>0 and ub<1:
+      x = p1[0] + ua*(p2[0]-p1[0])
+      y = p1[1] + ua*(p2[1]-p1[1])
+      return (1, [x,y])
+    return (0,)
+
+def adjacent_triangle(tri, ends): 
+  # tri and ends contain point indices
+  # returns adj triangle index and its normal
+  global triangles
+
+  for p in tri:
+    if not p in ends:
+      third = p
+
+  for i in range(len(triangles)):
+    t = triangles[i]
+    if (ends[0] in t) and (ends[1] in t) and (not third in t):
+      n = normals[i]
+      return (i,n)
+
+# https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+def rodrigues_rotation(n1, n2, v):
+  cross = np.cross(n1, n2)
+  if not cross.any():
+    return v
+  a = cross / np.linalg.norm(cross)
+  sinphi = np.linalg.norm(np.cross(n1/np.linalg.norm(n1), n2/np.linalg.norm(n2)))
+  cosphi = np.dot(n1/np.linalg.norm(n1), n2/np.linalg.norm(n2))
+  return v*cosphi + np.cross(a, v)*sinphi + a*np.dot(a, v)*(1-cosphi)
+
+def translation_3d(movement_2d, delta_yaw):
+    global unit
+    global loc
+    global tri
+    global vers
+
+    # unit_vectors = np.matmul([
+    # [math.cos(rotate), -math.sin(rotate), 0],
+    # [math.sin(rotate), math.cos(rotate), 0],
+    # [0,0,1]
+    # ], unit_vectors)
+    # u,v,w = unit_vectors
+
+    # unit_vectors = np.matmul([
+    # [math.cos(-rotate), -math.sin(-rotate), 0],
+    # [math.sin(-rotate), math.cos(-rotate), 0],
+    # [0,0,1]
+    # ], unit_vectors)
+    # u,v,w = unit_vectors
+
+    movement_vector = movement_2d.copy() + [0]
+    prev_vers = None
+
+    while not np.array_equal(movement_vector, [0,0,0]):
+        # calculate intersections
+        # edge1: v0 -> v1
+        if not np.array_equal(prev_vers, [points[vers[1]], points[vers[0]]]):
+            edge1_p0 = transform_coordinates(unit, points[vers[0]]-loc)
+            edge1_p1 = transform_coordinates(unit, points[vers[1]]-loc)
+            i1 = intersection_point([0,0,0], movement_vector, edge1_p0, edge1_p1)
+        else:
+            i1 = (0,)
+
+        # edge2: v1 -> v2
+        if not np.array_equal(prev_vers, [points[vers[2]], points[verse[2]]]):
+            edge2_p0 = transform_coordinates(unit, points[vers[1]]-loc)
+            edge2_p1 = transform_coordinates(unit, points[vers[2]]-loc)
+            i2 = intersection_point([0,0,0], movement_vector, edge2_p0, edge2_p1)
+        else:
+            i2 = (0,)
+
+        # edge3: v2 -> v0
+        if not np.array_equal(prev_vers, [points[vers[0]], points[vers[2]]]):
+            edge3_p0 = transform_coordinates(unit, points[vers[2]]-loc)
+            edge3_p1 = transform_coordinates(unit, points[vers[0]]-loc)
+            i3 = intersection_point([0,0,0], movement_vector, edge3_p0, edge3_p1)
+        else:
+            i3 = (0,)
+
+        # edge 1 intersection
+        if i1[0]==1 and i2[0]!=1 and i3[0]!=1:
+            prev_vers = [points[vers[0]], points[vers[1]]]
+
+            # update movement vector
+            movement_vector = movement_vector-(i1[1]+[0])
+            
+            # update location to intersection point
+            loc = point_local_to_global(unit, loc, i1[1]+[0])
+            
+            # calculate new triangle and vertices
+            tri, new_norm = adjacent_triangle(vers, [vers[0], vers[1]])
+            vers = triangles[tri]
+            
+            # calculate new unit vectors on new triangle
+            unit = np.array(list(map(lambda x: rodrigues_rotation(unit[2], new_norm, x), unit)))
+
+        # edge 2 intersection
+        elif i2[0]==1 and i1[0]!=1 and i3[0]!=1:
+            prev_vers = [points[vers[1]], points[vers[2]]]
+
+            # update movement vector
+            movement_vector = movement_vector-(i2[1]+[0])
+            
+            # update location to intersection point
+            loc = point_local_to_global(unit, loc, i2[1]+[0])
+            
+            # calculate new triangle and vertices
+            tri, new_norm = adjacent_triangle(vers, [vers[1], vers[2]])
+            vers = triangles[tri]
+            
+            # calculate new unit vectors on new triangle
+            unit = np.array(list(map(lambda x: rodrigues_rotation(unit[2], new_norm, x), unit)))
+
+        # edge 3 intersection
+        elif i3[0]==1 and i1[0]!=1 and i2[0]!=1:
+            prev_vers = [points[vers[2]], points[vers[0]]]
+
+            # update movement vector
+            movement_vector = movement_vector-(i3[1]+[0])
+            
+            # update location to intersection point
+            loc = point_local_to_global(unit, loc, i3[1]+[0])
+            
+            # calculate new triangle and vertices
+            tri, new_norm = adjacent_triangle(vers, [vers[2], vers[0]])
+            vers = triangles[tri]
+            
+            # calculate new unit vectors on new triangle
+            unit = np.array(list(map(lambda x: rodrigues_rotation(unit[2], new_norm, x), unit)))
+
+        elif i3[0]==0 and i2[0]==0 and i3[0]==0:
+            prev_vers = None
+            loc = point_local_to_global(unit, loc, movement_vector)
+            movement_vector = [0,0,0]
+
+        else:
+            print("uh oh")
+            break
+
+# 3d stuff
+print("Reading Head Model ...")
+points,normals,triangles = read_stl('tests/sphere.stl')
+tri = 0 # random starting triangle
+vers = triangles[tri]
+loc = points[vers[0]] # random starting location
+w = normals[tri]
+v = (points[vers[1]]-points[vers[0]]) / np.linalg.norm(points[vers[1]]-points[vers[0]])
+u = np.cross(v, w) / np.linalg.norm(np.cross(v, w))
+unit = np.array([u,v,w]) # random starting orientation
+del u,v,w
+
+# 2d stuff
 YAW_ZERO = imu.get_euler_angles()[2]
+pose2d = (0,0,0) # x,y,yaw
+ts = time.perf_counter() # timing performance
 print("IMU zeroed at {} deg.".format(YAW_ZERO))
 
-# x,y,heading
-pose = (0,0,0)
-ts = time.perf_counter()
-
 def main():
-    global RADIUS
-    global pose
+    global pose2d
     global ts
     global imu
 
-    # might need to do time matching
-    # optical waveform is fine but varies a lot with time
+    # NOTE: might need to do time matching to get imu and optical data to match up
+    #       though right now it doesn't seem necessary
     while True:
         imu.update()
-        new_yaw = imu.get_euler_angles()[2]
-        if new_yaw != pose[2]:
-            optical = get_displacement_vector()# we might be reading this even tho yaw hasn't changed so we're losing information
-            dP = global_translation(optical[0], optical[1], pose[2], new_yaw)
-            # dP = global_translation(optical[0], 0, pose[2], new_yaw)
-            # dP = global_translation(math.hypot(optical[0], optical[1]), 0, pose[2], new_yaw)
-            pose = (pose[0]+dP[0], pose[1]+dP[1], new_yaw)
 
-            with open('main.csv', 'a') as f:
-                writer = csv.writer(f, delimiter=',')
-                now = time.perf_counter()
-                writer.writerow([now, pose[0], pose[1], pose[2], optical[0], optical[1], (now-ts)*1000])
-                ts = now
-            
-            # print(pose)
+        old_yaw = pose2d[2]
+        new_yaw = imu.get_euler_angles()[2]
+        motion = get_displacement_vector()
+        dP = translation_2d(motion[0], motion[1], old_yaw, new_yaw)
+        pose2d = (pose2d[0]+dP[0], pose2d[1]+dP[1], new_yaw)
+        translation_3d(motion, 0)
+
+        print("tri: " + str(tri))
+        print("loc: " + str(loc))
+
+        with open('clipper.csv', 'a') as f:
+            writer = csv.writer(f, delimiter=',')
+            now = time.perf_counter()
+            writer.writerow([now, *pose2d, *loc, 0, *motion, (now-ts)*1000])
+            ts = now
 
 if __name__ == "__main__":
     main()
